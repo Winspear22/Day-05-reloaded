ba3ea8c5d4ec9aca869da6233447c634ccc22e23
Winspear22 <adnen.aloui2@hotmail.com>
2025-11-06 14:13:51 +0100

    PHP-05 - ex01 : création du service.

---
+++ ex01/src/Service/CreateTableService.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Service;
+
+class CreateTableService 
+{
+	public function __construct() {}
+
+	public function createTable(){}
+
+	public function checkTableExistence(){}
+}
+
+?>
