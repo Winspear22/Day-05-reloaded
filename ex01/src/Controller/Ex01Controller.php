ef8a41a3869d0d03ad7b40b6a42b892abdac814e
Winspear22 <adnen.aloui2@hotmail.com>
2025-11-08 14:11:47 +0100

    PHP-05 -- ex01: création du controller et du service.

--- ex01/src/Controller/Ex01Controller.php
+++ ex01/src/Controller/Ex01Controller.php
@@ -2,19 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Exception;
+use App\Service\CreateTableService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class Ex01Controller extends AbstractController
 {
+    public function __construct(
+        private readonly CreateTableService $tableCreator
+    ) {}
+
     /**
-     * @Route("ex01", name="ex01_index")
+     * @Route("ex01", name="ex01_index", methods={"GET"})
      */
     public function index(): Response
     {
-        return $this->render('ex01/index.html.twig', [
-            'controller_name' => 'Ex01Controller',
-        ]);
+        return $this->render('ex01/index.html.twig');
+    }
+
+    /**
+     * @Route("/ex01/create_table", name="ex01_create_table", methods={"POST"})
+     */
+    public function createTable(): Response
+    {
+        try
+        {
+            $result = $this->tableCreator->createTable('ex01_users');
+            if (str_starts_with($result, 'success'))
+                $this->addFlash('success', $result);
+            else
+                $this->addFlash('info', $result);
+            return $this->redirectToRoute('ex01_index');
+        }
+        catch (Exception $e)
+        {
+            $this->addFlash('danger', 'Error creating table: ' . $e->getMessage());
+            return $this->redirectToRoute('ex01_index');
+        }
     }
 }
--- ex01/src/Service/CreateTableService.php
+++ ex01/src/Service/CreateTableService.php
@@ -2,13 +2,51 @@
 
 namespace App\Service;
 
+use Exception;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Process\Process;
+
 class CreateTableService 
 {
-	public function __construct() {}
+	public function __construct(
+		private readonly Connection $orm_connection
+	) {}
 
-	public function createTable(){}
+	public function createTable(string $tableName): string
+	{
+		try
+		{
+			$result = $this->checkTableExistence($tableName);
+			if ($result === true)
+				return 'info: Table already exists.';
+			$process = new Process([
+				'php',
+				'bin/console',
+				'doctrine:migrations:migrate'
+			]);
+			$process->run();
+			if ($process->isSuccessful())
+				return 'success: Table created successfully!';
+			else
+				return 'error: popo' . $process->getErrorOutput();
+		}
+		catch (Exception $e)
+		{
+			return 'error: ' . $e->getMessage();
+		}
+	}
 
-	public function checkTableExistence(){}
+	public function checkTableExistence(string $tableName): bool
+	{
+		try
+		{
+			$schemaManager = $this->orm_connection->createSchemaManager();
+			return $schemaManager->tablesExist([$tableName]);
+		}
+		catch (Exception $e)
+		{
+			return false;
+		}
+	}
 }
-
 ?>
