e0cd238cfc51dfd1c4bb21ee526308f257e5523b
Winspear22 <adnen.aloui2@hotmail.com>
2025-11-10 11:59:22 +0100

    PHP-05 -- ex10: migration + service deleteORM.

---
+++ ex10/src/Service/InsertDataServiceORM.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use Exception;
+use App\Entity\Data;
+use App\Repository\DataRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class InsertDataServiceORM
+{
+	public function __construct(
+		private readonly DataRepository $data_repository,
+		private readonly EntityManagerInterface $em,
+		private readonly UtilsService $utilsService
+
+	) {}
+
+	public function insertDataORM(Data $data): string
+	{
+		try
+		{
+			if ($this->utilsService->checkTableExistenceORM('data'))
+			$this->em->persist($data);
+			$this->em->flush();
+			return "success: Data with ID " . $data->getId() . " inserted successfully!";
+		}
+		catch (Exception $e)
+		{
+			return "danger: Error inserting data with ID " . $data->getId() . ". " . $e->getMessage();
+		}
+	}
+}
