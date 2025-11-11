6fb4864c64882902f4054eec2e502a2cbd51f0f9
Winspear22 <adnen.aloui2@hotmail.com>
2025-11-11 15:39:46 +0100

    PHP-05 - ex12 : OK.

--- ex12/migrations/Version20251111121039.php
+++ ex12/migrations/Version20251111121039.php
@@ -23,8 +23,8 @@ final class Version20251111121039 extends AbstractMigration
         $this->addSql('CREATE TABLE ex12_addresses (id INT AUTO_INCREMENT NOT NULL, address LONGTEXT NOT NULL, person_id INT DEFAULT NULL, INDEX IDX_76403A1217BBB47 (person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('CREATE TABLE ex12_bank_accounts (id INT AUTO_INCREMENT NOT NULL, iban VARCHAR(34) NOT NULL, bank_name VARCHAR(50) NOT NULL, person_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_58BC9EE7217BBB47 (person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('CREATE TABLE ex12_persons (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(64) NOT NULL, name VARCHAR(64) NOT NULL, email VARCHAR(128) NOT NULL, enable TINYINT(1) NOT NULL, birthdate DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE ex12_addresses ADD CONSTRAINT FK_76403A1217BBB47 FOREIGN KEY (person_id) REFERENCES ex12_persons (id)');
-        $this->addSql('ALTER TABLE ex12_bank_accounts ADD CONSTRAINT FK_58BC9EE7217BBB47 FOREIGN KEY (person_id) REFERENCES ex12_persons (id)');
+        $this->addSql('ALTER TABLE ex12_addresses ADD CONSTRAINT FK_76403A1217BBB47 FOREIGN KEY (person_id) REFERENCES ex12_persons (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE ex12_bank_accounts ADD CONSTRAINT FK_58BC9EE7217BBB47 FOREIGN KEY (person_id) REFERENCES ex12_persons (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
